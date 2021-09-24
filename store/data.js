95a2523bdd85f884f9c1233f5d922faaf3d32dfd
teshdev <gebrewold2012@gmail.com>
2021-09-24 13:30:05 -0700

    first commit

---
+++ store/data.js
@@ -0,0 +1,38 @@
+// A collection of musicians where the key is the unique id
+module.exports = {
+  coltrane: {
+    firstName: 'Ester',
+    lastName: 'Awoke',
+    genre: 'JAZZ',
+  },
+
+  davis: {
+    firstName: 'Teddy',
+    lastName: 'Afro',
+    genre: 'Rock',
+  },
+
+  mccartney: {
+    firstName: 'Ephrem',
+    lastName: 'Tamiru',
+    genre: 'ROCK',
+  },
+
+  hendrix: {
+    firstName: 'Tilahun',
+    lastName: 'Gesese',
+    genre: 'Rock',
+  },
+
+  cobain: {
+    firstName: 'Bisrat',
+    lastName: 'Surafel',
+    genre: 'Rock',
+  },
+
+  king: {
+    firstName: 'Lij',
+    lastName: 'Mickael',
+    genre: 'Rock',
+  },
+};
